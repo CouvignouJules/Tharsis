60e360b1f14f297f54d716def57ff87e399a2ba5
PC-Jules <PC-Jules@PC-JULES>
2017-02-21 15:30:59 +0100

    crea dice tour

---
+++ Tharsis/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dice;
+using Equipage;
+using Vaisseau;
+
+namespace Tharsis
+{
+    class Tour
+    {
+        
+    }
+}
