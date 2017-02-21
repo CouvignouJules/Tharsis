a136394696cbdef9dec797e2d5f24bbcceb142ce
PC-Jules <PC-Jules@PC-JULES>
2017-02-21 11:26:19 +0100

    first

---
+++ Equipage/Equipage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipage
+{
+    abstract class Equipage
+    {
+        int pv;
+        int des;
+        Random rnd = new Random();
+        public int pif()
+        {
+            return rnd.Next(2,4);
+        }
+
+        public abstract void toString();
+    }
+}
