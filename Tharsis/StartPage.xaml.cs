55ff0bf6572f5b437f03bc8718124147669c08dc
Thomas <Thomas@DESKTOP-1EUQUU4>
2017-03-14 17:09:02 +0100

    todo hard mode

--- Tharsis/StartPage.xaml.cs
+++ Tharsis/StartPage.xaml.cs
@@ -39,7 +39,11 @@ namespace Tharsis
 
         private void hard_Click(object sender, RoutedEventArgs e)
         {
-
+            /*TODO: 
+                - Page Hardmode
+                - Mode difficile
+             
+             */
         }
     }
 }
